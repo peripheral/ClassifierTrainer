7ad2540b91f220fd45b90fa0bac24fb5451f5d39
Artur Vitt <dekster13@hotmail.com>
2015-11-04 15:35:34 +0100

    Revert "Revert "Math utilities, calculate amplitude of a vector ""
    This reverts commit 974ca3e40365e6355deecbe7007f74cf234a6698.

--- src/app/FeatureVectorExport.java
+++ src/app/FeatureVectorExport.java
@@ -40,59 +40,59 @@ public class FeatureVectorExport {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public void export(Pose pose){	
 		try {
 			/* Writing down head */
-			int head[] = pose.convertToKinectCoordinates(pose.getHead());
+			int head[] = CameraModel.convertToKinectCoordinates(pose,pose.getHead());
 			outI.append(head[0]+",");outI.append(head[1]+",");outI.append(head[2]+",");
 			/* Writing down Shoulder center */
-			int sc[] = pose.convertToKinectCoordinates(pose.getShoulder_center());
+			int sc[] = CameraModel.convertToKinectCoordinates(pose,pose.getShoulder_center());
 			outI.append(sc[0]+",");outI.append(sc[1]+",");outI.append(sc[2]+",");
 			/* Writing down right shoulder */
-			int sr[] = pose.convertToKinectCoordinates(pose.getShoulder_right());
+			int sr[] = CameraModel.convertToKinectCoordinates(pose,pose.getShoulder_right());
 			outI.append(sr[0]+",");outI.append(sr[1]+",");outI.append(sr[2]+",");
 			/* Writing down left shoulder */
-			int sl[] = pose.convertToKinectCoordinates(pose.getShoulder_left());
+			int sl[] = CameraModel.convertToKinectCoordinates(pose,pose.getShoulder_left());
 			outI.append(sl[0]+",");outI.append(sl[1]+",");outI.append(sl[2]+",");
 			/* Writing down spine */
-			int s[] = pose.convertToKinectCoordinates(pose.getSpine());
+			int s[] = CameraModel.convertToKinectCoordinates(pose,pose.getSpine());
 			outI.append(s[0]+",");outI.append(s[1]+",");outI.append(s[2]+",");
 			/* Writing down left elbow*/
-			int el[] = pose.convertToKinectCoordinates(pose.getElbow_left());
+			int el[] = CameraModel.convertToKinectCoordinates(pose,pose.getElbow_left());
 			outI.append(el[0]+",");outI.append(el[1]+",");outI.append(el[2]+",");
 			/* Writing down elbow right*/
-			int er[] = pose.convertToKinectCoordinates(pose.getElbow_right());
+			int er[] = CameraModel.convertToKinectCoordinates(pose,pose.getElbow_right());
 			outI.append(er[0]+",");outI.append(er[1]+",");outI.append(er[2]+",");
 			/* Writing down left wrist */
-			int wl[] = pose.convertToKinectCoordinates(pose.getWrist_left());
+			int wl[] = CameraModel.convertToKinectCoordinates(pose,pose.getWrist_left());
 			outI.append(wl[0]+",");outI.append(wl[1]+",");outI.append(wl[2]+",");
 			/* Writing down right wrist */
-			int wr[] = pose.convertToKinectCoordinates(pose.getWrist_right());
+			int wr[] = CameraModel.convertToKinectCoordinates(pose,pose.getWrist_right());
 			outI.append(wr[0]+",");outI.append(wr[1]+",");outI.append(wr[2]+",");
 			/* Writing down left hip */
-			int hl[] = pose.convertToKinectCoordinates(pose.getHip_left());
+			int hl[] = CameraModel.convertToKinectCoordinates(pose,pose.getHip_left());
 			outI.append(hl[0]+",");outI.append(hl[1]+",");outI.append(hl[2]+",");
 			/* Writing down right hip */
-			int hr[] = pose.convertToKinectCoordinates(pose.getHip_right());
+			int hr[] = CameraModel.convertToKinectCoordinates(pose,pose.getHip_right());
 			outI.append(hr[0]+",");outI.append(hr[1]+",");outI.append(hr[2]+",");
 			/* Writing down head */
-			int kl[] = pose.convertToKinectCoordinates(pose.getKnee_left());
+			int kl[] = CameraModel.convertToKinectCoordinates(pose,pose.getKnee_left());
 			outI.append(kl[0]+",");outI.append(kl[1]+",");outI.append(kl[2]+",");
 			/* Writing down head */
-			int kr[] = pose.convertToKinectCoordinates(pose.getKnee_right());
+			int kr[] = CameraModel.convertToKinectCoordinates(pose,pose.getKnee_right());
 			outI.append(kr[0]+",");outI.append(kr[1]+",");outI.append(kr[2]+",");
 			/* Writing down head */
-			int al[] = pose.convertToKinectCoordinates(pose.getAnkle_left());
+			int al[] = CameraModel.convertToKinectCoordinates(pose,pose.getAnkle_left());
 			outI.append(al[0]+",");outI.append(al[1]+",");outI.append(al[2]+",");
 			/* Writing down head */
-			int ar[] = pose.convertToKinectCoordinates(pose.getAnkle_right());
+			int ar[] = CameraModel.convertToKinectCoordinates(pose,pose.getAnkle_right());
 			outI.append(ar[0]+",");outI.append(ar[1]+",");outI.append(ar[2]+",");
 			/* Writing down head */
-			int fl[] = pose.convertToKinectCoordinates(pose.getFoot_left());
+			int fl[] = CameraModel.convertToKinectCoordinates(pose,pose.getFoot_left());
 			outI.append(fl[0]+",");outI.append(fl[1]+",");outI.append(fl[2]+",");
 			/* Writing down head */
-			int fr[] = pose.convertToKinectCoordinates(pose.getFoot_right());
+			int fr[] = CameraModel.convertToKinectCoordinates(pose,pose.getFoot_right());
 			outI.append(fr[0]+",");outI.append(fr[1]+",");outI.append(fr[2]+"");
 			outI.append("\n");
 		} catch (IOException e) {
